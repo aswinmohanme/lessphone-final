1c2afe5e91774411c1c407070706ff5ca5e016df
aswinmohanme <dev.aswinmohan@gmail.com>
2020-08-11 16:15:36 +0530

    Set Dark Bottom Bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:in_app_purchase/in_app_purchase.dart';
+import 'package:flutter/services.dart';
 
 import './screens/home_screen.dart';
 import 'themes.dart';
@@ -10,6 +11,8 @@ import './models/settings_box.dart';
 void main() async {
   await HiveRepo.initHive();
   InAppPurchaseConnection.enablePendingPurchases();
+
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark);
   runApp(Lessphone());
 }
 
